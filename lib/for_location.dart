3344652d226d2feab7731f7346bf7b7d564e1622
HtayWaiYanAung <dontgun54@gmail.com>
2025-05-10 16:03:15 +0630

    update changes for get cureent location on map with mark

--- lib/for_location.dart
+++ lib/for_location.dart
@@ -4,7 +4,7 @@ import 'package:map_map/locat_notifier.dart';
 import 'package:provider/provider.dart';
 
 class ForLocation extends StatelessWidget {
-   ForLocation({super.key});
+  ForLocation({super.key});
 
   final _formKey = GlobalKey<FormState>();
 
@@ -14,14 +14,18 @@ class ForLocation extends StatelessWidget {
       create: (context) => LocatNotifier()..initialLocationCamera(),
       child: Consumer<LocatNotifier>(
         builder: (context, provoider, child) {
-          if (provoider.initialCamera == null){
-            return Center(child: CircularProgressIndicator(),);
+          if (provoider.initialCamera == null) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
           }
           return Scaffold(
-            body: SizedBox(
-              child: Column(
-                children: [
-                  Expanded(
+              body: Stack(
+            children: [
+              SizedBox(
+                child: Column(
+                  children: [
+                    Expanded(
                       child: FlutterMap(
                           mapController: provoider.mapController,
                           options: MapOptions(
@@ -35,7 +39,7 @@ class ForLocation extends StatelessWidget {
                                 pinchMoveThreshold: 40.0,
                                 flags: InteractiveFlag.pinchZoom |
                                     InteractiveFlag.drag |
-                                    InteractiveFlag.rotate |
+                                    // InteractiveFlag.rotate |
                                     InteractiveFlag.doubleTapDragZoom |
                                     InteractiveFlag.scrollWheelZoom),
                             onTap: (_, point) {
@@ -43,156 +47,184 @@ class ForLocation extends StatelessWidget {
                             },
                           ),
                           children: [
-                        TileLayer(
-                          urlTemplate:
-                              'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                          subdomains: ['a', 'b', 'c'],
-                        ),
-                        if (provoider.pickLatLng != null)
-                          MarkerLayer(markers: [
-                            Marker(
-                                point: provoider.pickLatLng!,
-                                height: 40,
-                                width: 40,
-                                child: Icon(
-                                  Icons.heart_broken,
-                                  color: Colors.pinkAccent.shade100,
-                                  size: 38,
-                                ))
-                          ])
-                      ])),
-                      if (provoider.isbusy) LinearProgressIndicator(),
-                      Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      TextFormField(
-                          controller: provoider.addressLine,
-                          decoration: InputDecoration(
-                              fillColor: Colors.white,
-                              hintText: 'Address Line',
-                              hintStyle: TextStyle(
-                                  fontWeight: FontWeight.w400,
-                                  color: Colors.black)),
-                          validator: (value) => (value == null || value.isEmpty)
-                              ? 'Required'
-                              : null),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: TextFormField(
-                              controller: provoider.city,
-                              decoration: InputDecoration(
-                                  hintText: 'City',
-                                  hintStyle: TextStyle(
-                                      fontWeight: FontWeight.w400,
-                                      color: Colors.black)),
-                              validator: (value) =>
-                                  (value == null || value.isEmpty)
-                                      ? 'Required'
-                                      : null,
+                            TileLayer(
+                              urlTemplate:
+                                  'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                              subdomains: ['a', 'b', 'c'],
                             ),
-                          ),
-                          SizedBox(
-                            width: 20,
-                          ),
-                          Expanded(
-                            child: TextFormField(
-                              controller: provoider.state,
-                              decoration: InputDecoration(
-                                  hintText: 'State',
-                                  hintStyle: TextStyle(
-                                      fontWeight: FontWeight.w400,
-                                      color: Colors.black)),
-                              validator: (value) =>
-                                  (value == null || value.isEmpty)
-                                      ? 'Required'
-                                      : null,
-                            ),
-                          ),
-                        ],
+                            if (provoider.pickLatLng != null)
+                              MarkerLayer(markers: [
+                                Marker(
+                                    point: provoider.pickLatLng!,
+                                    child: Icon(
+                                      Icons.heart_broken,
+                                      color: Colors.pinkAccent.shade100,
+                                      size: 38,
+                                    ))
+                              ]),
+                          ]),
+                    ),
+                    if (provoider.isbusy) LinearProgressIndicator(),
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 14,vertical: 6),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20)
                       ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: TextFormField(
-                              controller: provoider.country,
-                              decoration: InputDecoration(
-                                  hintText: 'Country',
-                                  hintStyle: TextStyle(
-                                      fontWeight: FontWeight.w400,
-                                      color: Colors.black)),
-                              validator: (value) =>
-                                  (value == null || value.isEmpty)
-                                      ? 'Required'
-                                      : null,
-                            ),
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Expanded(
-                            child: TextFormField(
-                              controller: provoider.postalCode,
-                              keyboardType: TextInputType.number,
-                              decoration: InputDecoration(
-                                  hintText: 'Postal Code',
-                                  hintStyle: TextStyle(
-                                      fontWeight: FontWeight.w400,
-                                      color: Colors.black)),
-                              validator: (value) {
-                                if (value == null || value.isEmpty)
-                                  return 'Required';
-                                if (int.tryParse(value) == null)
-                                  return 'Postal Code required number';
-                                if (value.length <5)
-                                return 'Postal code must be at least 5 digits';
-                              },
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            padding: EdgeInsets.symmetric(vertical: 2),
-                              fixedSize: Size(360, 45),
-                              side: BorderSide(
-                                  color: Color.fromARGB(255, 184, 184, 184)),
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10)),
-                              backgroundColor: Colors.blue.shade600,
-                              foregroundColor: Colors.white),
-                          onPressed: ()async {
-                            // if (_formKey.currentState!.validate()) {
-                            //   provider.addNewAddress();
-                            //   _formKey.currentState!.reset();
-                            //   provider.addressLine.clear();
-                            //   provider.city.clear();
-                            //   provider.state.clear();
-                            //   provider.country.clear();
-                            //   provider.postalCode.clear();
-                            //   AppNavigator.pop(context);
-                            // }
-                          },
-                          child: Text(
-                            'Add Address',
-                            style: TextStyle(fontSize: 16,),
-                          ))
-                    ],
-                  )),
-                ],
+                      child: Form(
+                          key: _formKey,
+                          child: Column(
+                            children: [
+                              TextFormField(
+                                  controller: provoider.addressLine,
+                                  decoration: InputDecoration(
+                                      fillColor: Colors.white,
+                                      hintText: 'Address Line',
+                                      hintStyle: TextStyle(
+                                          fontWeight: FontWeight.w400,
+                                          color: Colors.black)),
+                                  validator: (value) =>
+                                      (value == null || value.isEmpty)
+                                          ? 'Required'
+                                          : null),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: TextFormField(
+                                      controller: provoider.city,
+                                      decoration: InputDecoration(
+                                          hintText: 'City',
+                                          hintStyle: TextStyle(
+                                              fontWeight: FontWeight.w400,
+                                              color: Colors.black)),
+                                      validator: (value) =>
+                                          (value == null || value.isEmpty)
+                                              ? 'Required'
+                                              : null,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 20,
+                                  ),
+                                  Expanded(
+                                    child: TextFormField(
+                                      controller: provoider.state,
+                                      decoration: InputDecoration(
+                                          hintText: 'State',
+                                          hintStyle: TextStyle(
+                                              fontWeight: FontWeight.w400,
+                                              color: Colors.black)),
+                                      validator: (value) =>
+                                          (value == null || value.isEmpty)
+                                              ? 'Required'
+                                              : null,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: TextFormField(
+                                      controller: provoider.country,
+                                      decoration: InputDecoration(
+                                          hintText: 'Country',
+                                          hintStyle: TextStyle(
+                                              fontWeight: FontWeight.w400,
+                                              color: Colors.black)),
+                                      validator: (value) =>
+                                          (value == null || value.isEmpty)
+                                              ? 'Required'
+                                              : null,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  Expanded(
+                                    child: TextFormField(
+                                      controller: provoider.postalCode,
+                                      keyboardType: TextInputType.number,
+                                      decoration: InputDecoration(
+                                          hintText: 'Postal Code',
+                                          hintStyle: TextStyle(
+                                              fontWeight: FontWeight.w400,
+                                              color: Colors.black)),
+                                      validator: (value) {
+                                        if (value == null || value.isEmpty)
+                                          return 'Required';
+                                        if (int.tryParse(value) == null)
+                                          return 'Postal Code required number';
+                                        if (value.length < 5)
+                                          return 'Postal code must be at least 5 digits';
+                                      },
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                      padding: EdgeInsets.symmetric(vertical: 2),
+                                      fixedSize: Size(360, 45),
+                                      side: BorderSide(
+                                          color:
+                                              Color.fromARGB(255, 184, 184, 184)),
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10)),
+                                      backgroundColor: Colors.blue.shade600,
+                                      foregroundColor: Colors.white),
+                                  onPressed: () async {
+                                    // if (_formKey.currentState!.validate()) {
+                                    //   provider.addNewAddress();
+                                    //   _formKey.currentState!.reset();
+                                    //   provider.addressLine.clear();
+                                    //   provider.city.clear();
+                                    //   provider.state.clear();
+                                    //   provider.country.clear();
+                                    //   provider.postalCode.clear();
+                                    //   AppNavigator.pop(context);
+                                    // }
+                                  },
+                                  child: Text(
+                                    'Add Address',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                    ),
+                                  ))
+                            ],
+                          )),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          );
+              Positioned(
+                width: 42,
+                height: 42,
+                bottom: 270,
+                right: 18,
+                child: FloatingActionButton(
+                  heroTag: 'recenter',
+                  onPressed: () {
+                    final ctl = provoider.mapController!;
+                    final center = provoider.initialCamera!;
+                    final zoom = provoider.initialZoom!;
+
+                    ctl.move(center, zoom);
+                  },
+                  child: const Icon(Icons.my_location,size: 30,color: Colors.blue,),
+                ),
+              )
+            ],
+          ));
         },
       ),
     );
--- lib/locat_notifier.dart
+++ lib/locat_notifier.dart
@@ -26,9 +26,11 @@ class LocatNotifier extends ChangeNotifier{
 
   Future<Position> getLocationPermission() async {
     try {
+
       if (!await Geolocator.isLocationServiceEnabled()){
       throw Exception('Location service is deniced');
     }
+
     var check = await Geolocator.checkPermission();
     if (check == LocationPermission.denied){
       check = await Geolocator.requestPermission();
@@ -36,6 +38,7 @@ class LocatNotifier extends ChangeNotifier{
         throw Exception('Location service is deniced');
       }
     }
+    
     if (check == LocationPermission.deniedForever){
       await Geolocator.openAppSettings();
       throw Exception('Location service is deniced');
@@ -52,11 +55,61 @@ class LocatNotifier extends ChangeNotifier{
     }
   }
 
+  // Future<Position> getLocationPermission() async {
+  //   try {
+
+  //     if (!await Geolocator.isLocationServiceEnabled()){
+  //     throw Exception('Location service is deniced');
+  //   }
+
+  //   var check = await Permission.location.status;
+  //   if (check.isDenied){
+  //     check = await Permission.location.request();
+  //     if (!check.isGranted){
+  //       throw Exception('Location service is deniced');
+  //     }
+  //   }
+
+  //   if (await Permission.location.isPermanentlyDenied){
+  //     await openAppSettings();
+  //     throw Exception('Location service are permanently deniced');
+  //   }
+  //   return Geolocator.getCurrentPosition(
+  //     locationSettings: LocationSettings(
+  //       accuracy: LocationAccuracy.high,
+  //       distanceFilter: 0
+  //     )
+  //   );
+  //   }
+  //   catch (e) {
+  //     throw Exception('Location service is dencied by user!');
+  //   }
+  // }
+
   Future<void> initialLocationCamera() async {
     try {
       final currentLocation = await getLocationPermission();
       _initialCamera = LatLng(currentLocation.latitude, currentLocation.longitude);
       _initialZoom = 16.0;
+
+      _pickLatLng = _initialCamera;
+      notifyListeners();
+
+      final places = await placemarkFromCoordinates(_pickLatLng!.latitude, _pickLatLng!.longitude);
+
+      if (places.isNotEmpty){
+        final placeMark = places.first;
+
+        final number = placeMark.subThoroughfare ?? '';
+        final street = placeMark.thoroughfare ?? '';
+
+        // addressLine.text =  [street,number].where((s) => s.isNotEmpty).join('');
+        addressLine.text = '${placeMark.street ?? ''} ${placeMark.name ?? ''}'.trim();
+        city.text = placeMark.locality ?? '';
+        state.text = placeMark.administrativeArea ?? '';
+        country.text = placeMark.country ?? '';
+        postalCode.text = placeMark.postalCode ?? '';
+      }
       
     }
     catch (_){
@@ -80,10 +133,8 @@ class LocatNotifier extends ChangeNotifier{
         final number = placeMark.subThoroughfare ?? '';
         final street = placeMark.thoroughfare ?? '';
 
-        addressLine.text =  [
-          if (street.isNotEmpty) street,
-          if (number.isNotEmpty) number,
-        ].join('');
+        // addressLine.text =  [street,number].where((s) => s.isNotEmpty).join('');
+        addressLine.text = '${placeMark.street ?? ''} ${placeMark.name ?? ''}'.trim();
         city.text = placeMark.locality ?? '';
         state.text = placeMark.administrativeArea ?? '';
         country.text = placeMark.country ?? '';
